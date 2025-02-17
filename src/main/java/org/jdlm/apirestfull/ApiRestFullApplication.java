5af9a330f80ca60b7b5fe93ceb644ff087646c66
jorneycr <jorneycr@hotmail.com>
2025-02-16 22:09:54 -0600

    first commit

---
+++ src/main/java/org/jdlm/apirestfull/ApiRestFullApplication.java
@@ -0,0 +1,13 @@
+package org.jdlm.apirestfull;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiRestFullApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiRestFullApplication.class, args);
+    }
+
+}
